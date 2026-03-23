40fcf09d4e21393b4b978826b14797e55c5fbb50
Ruan Queiroz <ruanqueirozdev@gmail.com>
2026-03-23 11:46:31 -0300

    criacao do script da aplicacao junto com o preload

---
+++ preload.js
@@ -0,0 +1,7 @@
+﻿const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('mhq', {
+  openComicFile: () => ipcRenderer.invoke('dialog:open-comic-file'),
+  loadComic: (filePath) => ipcRenderer.invoke('comic:load', filePath),
+  getPdfJsPaths: () => ipcRenderer.invoke('pdfjs:get-paths')
+});
